20ffa3ad2735a0fc44d02913faa3818f9febf4b5
paulincai <paulailincai@icloud.com>
2018-12-30 12:46:12 +0400

    Library updates with very small insignifiant tweaks.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 /* globals Package, Npm, Cordova */
 Package.describe({
   name: 'activitree:meteor-push-notifications',
-  version: '3.3.3',
+  version: '3.3.4',
   summary: 'Push notifications for APN and Firebase Admin (FCM)',
   git: 'https://github.com/activitree/meteor-push-notifications.git'
 })
 
 Npm.depends({
   'apn': '3.0.0-alpha1',
-  'firebase-admin': '6.1.0'
+  'firebase-admin': '6.4.0'
 })
 
 Cordova.depends({
