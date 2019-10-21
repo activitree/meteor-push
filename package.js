a83d19577c83cf8754111826982d773d7b138064
paulincai <paulailincai@icloud.com>
2019-10-21 23:32:00 +0400

    Initialize Firebase with configuration, hooks for sub/unsub and example

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 /* globals Package, Npm, Cordova */
 Package.describe({
-  name: 'activitree:push',
+  name: 'push-local',
   version: '1.0.1',
-  summary: 'Push notifications for APN and Firebase Admin (FCM) (Mobile only)',
+  summary: 'Push notifications for APN and Firebase (FCM) (with web-push)',
   git: 'https://github.com/activitree/meteor-push.git'
 })
 
 Npm.depends({
-  'apn': '3.0.0-alpha1',
-  'firebase-admin': '8.6.1'
+  apn: '3.0.0-alpha1',
+  'firebase-admin': '8.6.1',
+  firebase: '7.2.1'
 })
 
 Cordova.depends({
@@ -21,7 +22,7 @@ Package.onUse(api => {
   api.use(['ecmascript'])
 
   api.use(['tracker'], ['web.browser', 'web.cordova'])
-  api.use(['accounts-base'], ['web.cordova', 'server'], { weak: true })
+  api.use(['accounts-base'], ['web.browser', 'web.cordova', 'server'], { weak: true })
 
   api.use([
     'raix:eventstate@0.0.5',
