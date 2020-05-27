ad929c406914a690c96370d6282b883c82d654a3
Paul Ailincai <paulailincai@icloud.com>
2020-05-27 10:17:19 +0400

    Update sample service worker example
    Updated the Firebase library version.

--- example/app/public/firebase-messaging-sw.js
+++ example/app/public/firebase-messaging-sw.js
@@ -1,10 +1,15 @@
 // import firebase scripts inside service worker js script
-importScripts('https://www.gstatic.com/firebasejs/7.2.2/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/7.2.2/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/7.14.5/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/7.14.5/firebase-messaging.js')
 
 // This worker is being registerd by the Web component in Activitree Meteor Push - Client
 
-firebase.initializeApp({ messagingSenderId: 'xxxxxxxx' }) // get this from your Firebase project
+firebase.initializeApp({
+  projectId: 'xxxxx',
+  apiKey: 'xxxxxx',
+  appId: 'xxxxxxx',
+  messagingSenderId: 'xxxxxxxxxx'
+}) // get this from your Firebase project
 
 const messaging = firebase.messaging()
 
