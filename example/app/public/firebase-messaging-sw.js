95daad4975ec47695e5afa77b473ec154175d81a
Paul Ailincai <paulailincai@icloud.com>
2021-08-08 19:04:45 +0000

    Update worker libraries

--- example/app/public/firebase-messaging-sw.js
+++ example/app/public/firebase-messaging-sw.js
@@ -1,17 +1,39 @@
 // import firebase scripts inside service worker js script
-importScripts('https://www.gstatic.com/firebasejs/7.14.5/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/7.14.5/firebase-messaging.js')
+if (firebase && firebase.messaging && firebase.messaging.isSupported()) {
+  importScripts('https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js')
+  importScripts('https://www.gstatic.com/firebasejs/8.9.0/firebase-messaging.js')
 
 // This worker is being registerd by the Web component in Activitree Meteor Push - Client
 
-firebase.initializeApp({
-  projectId: 'xxxxx',
-  apiKey: 'xxxxxx',
-  appId: 'xxxxxxx',
-  messagingSenderId: 'xxxxxxxxxx'
-}) // get this from your Firebase project
+  firebase.initializeApp({
+    apiKey: 'xxxxxxx',
+    authDomain: 'xxxxxxx',
+    projectId: 'xxxxxxx',
+    storageBucket: 'xxxxxxx',
+    messagingSenderId: 'xxxxxxx',
+    appId: 'xxxxxxx',
+    measurementId: 'xxxxxxx',
+  }) // get this from your Firebase project
 
-const messaging = firebase.messaging()
+   const messaging = firebase.messaging()
+
+    /*
+    Debug: printout a received notification in console.
+    messaging.onBackgroundMessage(payload => {
+      console.log('[firebase-messaging-sw.js] Received background message ', payload)
+      // Customize notification here
+      const notificationTitle = payload.notification.title
+      const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon,
+        image: payload.notification.image,
+        action: payload.notification.click_action || payload.fcmOptions.link
+      }
+
+      self.registration.showNotification(notificationTitle, notificationOptions)
+    })
+   */
+}
 
 self.addEventListener('notificationclick', event => {
   if (event.action) {
